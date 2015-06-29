37d0c81e6b4d3247eb9661fbbf1961aa3736f6f4
Vyacheslav Slinko <vslinko@yahoo.com>
2015-06-29 15:02:03 +0300

    Fix cookieAuthMiddleware

--- src/webserver/di/cookieAuthMiddleware.js
+++ src/webserver/di/cookieAuthMiddleware.js
@@ -13,21 +13,35 @@ export default async function cookieAuthMiddleware(request, response, next) {
       .where({hash: request.cookies.accessToken})
       .one()
 
+    if (!token) {
+      return
+    }
+
     const tokenResource = await mapObjectToResource(orientoDb, token)
+
+    if (tokenResource.relationships.user.data.length < 1) {
+      return
+    }
 
     const user = await orientoDb
       .select()
       .from('User')
       .where({
-        '@rid': request.tokenResource.relationships.user.data[0].id // TODO: fix
+        '@rid': tokenResource.relationships.user.data[0].id
       })
       .one()
+
+    if (!user) {
+      return
+    }
 
     const userResource = await mapObjectToResource(orientoDb, user)
 
     request.tokenResource = tokenResource
     request.userResource = userResource
-  } finally {
-    next()
+  } catch (error) {
+    return next(error)
   }
+
+  next()
 }
